c6f37d415875b08a1c47998b9a114383d41688fa
rafaucoin <mohameddaouha@gmail.com>
2022-04-17 22:06:43 +0100

    navbar logo

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -33,7 +33,15 @@ const NavBar = () => {
         fontWeight="bold"
         paddingLeft={[0, 2, 5, 5]}
       >
-        <Link href="/">Realtor</Link>
+        <Link href="/">
+          <Flex gap={1} alignItems="center">
+        <svg
+        height="30"
+        width="30"
+        class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+        Realtor
+          </Flex>
+        </Link>
       </Box>
       <Spacer />
       <Flex display={{base:"none",md:"flex"}} fontSize={["lg", "sm"]} paddingLeft={["13", 2, 0, 0]}>

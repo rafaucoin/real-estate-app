3be391d78fe20267419dce918cdd6c2c6afc2c28
rafaucoin <88778777+rafaucoin@users.noreply.github.com>
2022-06-27 18:54:44 +0100

    playing with lazy loading

--- components/Caroussel.jsx
+++ components/Caroussel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Box,
   IconButton,
@@ -9,12 +9,12 @@ import {
   Container,
   chakra,
   Flex,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 // Here we have used react-icons package for the icons
-import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
+import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 // And react-slick as our Caroussel Lib
-import Slider from 'react-slick';
-import Image from 'next/image';
+import Slider from "react-slick";
+import Image from "next/image";
 
 // Settings for the slider
 
@@ -33,34 +33,35 @@ const Images = chakra(Image, {
       "layout",
     ].includes(prop),
 });
-export default function Caroussel({photos}) {
+export default function Caroussel({ photos }) {
   // As we have used custom buttons, we need a reference variable to
   // change the state
   const [slider, setSlider] = React.useState(null);
 
   // These are the breakpoints which changes the position of the
   // buttons as the screen size changes
-  const top = useBreakpointValue({ base: '50%', md: '50%' });
-  const side = useBreakpointValue({ base: '5%', md: '40px' });
+  const top = useBreakpointValue({ base: "50%", md: "50%" });
+  const side = useBreakpointValue({ base: "5%", md: "40px" });
   const settings = {
     dots: true,
     arrows: false,
-    lazyLoad: true,  infinite: true,
+    lazyLoad: false,
+    infinite: true,
     autoplay: true,
     speed: 500,
     autoplaySpeed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    initialSlide: Math.floor(photos.length/2)
+    initialSlide: Math.floor(photos.length / 2),
   };
-  
 
   return (
     <Box
-      position={'relative'}
-      height={'100%'}
-      width={'full'}
-      overflow={'hidden'}>
+      position={"relative"}
+      height={"100%"}
+      width={"full"}
+      overflow={"hidden"}
+    >
       {/* CSS files for react-slick */}
       <link
         rel="stylesheet"
@@ -80,9 +81,10 @@ export default function Caroussel({photos}) {
         position="absolute"
         left={side}
         top={top}
-        transform={'translate(0%, -50%)'}
+        transform={"translate(0%, -50%)"}
         zIndex={2}
-        onClick={() => slider?.slickPrev()}>
+        onClick={() => slider?.slickPrev()}
+      >
         <BiLeftArrowAlt size="40px" />
       </IconButton>
       {/* Right Icon */}
@@ -93,9 +95,10 @@ export default function Caroussel({photos}) {
         right={side}
         top={top}
         borderRadius={10}
-        transform={'translate(0%, -50%)'}
+        transform={"translate(0%, -50%)"}
         zIndex={2}
-        onClick={() => slider?.slickNext()}>
+        onClick={() => slider?.slickNext()}
+      >
         <BiRightArrowAlt size="40px" />
       </IconButton>
       {/* Slider */}
@@ -103,11 +106,17 @@ export default function Caroussel({photos}) {
         {photos?.map((photo) => (
           <Box
             key={photo.id}
-            height={{base:'55vh', md:"70vh"}}
+            height={{ base: "55vh", md: "70vh" }}
             position="relative"
-            >
+          >
             {/* This is the block you need to change, to customize the caption */}
-            <Images key={photo.id} src={photo.url} objectFit='cover' borderRadius={10} layout='fill' />
+            <Images
+              key={photo.id}
+              src={photo.url}
+              objectFit="cover"
+              borderRadius={10}
+              layout="fill"
+            />
           </Box>
         ))}
       </Slider>
